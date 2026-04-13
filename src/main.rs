b8a24f4d8cf169ea31b1ed12cba4de09b1ca63d9
panicdragon <d.takayama.web@gmail.com>
2026-04-13 19:39:39 +0900

    refactor: project name

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ use eframe::egui;
 fn main() -> eframe::Result {
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_title("r-chr")
+            .with_title("R-CHR")
             .with_inner_size([800.0, 600.0]),
         ..Default::default()
     };
 
     eframe::run_native(
-        "r-chr",
+        "R-CHR",
         options,
         Box::new(|cc| {
             app::setup_fonts(&cc.egui_ctx);
